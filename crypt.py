5beeafaa9c4a474bd689335163db1d586995fb73
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-11-02 13:57:26 +0530

    merged

--- crypt.py
+++ crypt.py
@@ -4,41 +4,47 @@ class payload:
         self.key = key
         self.encrypted = encrypted
     
-    def cipherText(self):
-        special_char = list('[@_!#$%^&*()<>?/\|}{~:+-]')
+    def encrypt(self):
+        special_char = list('[@_!#$%^&*()<>?/\\|}{~:+-]')
         result = ""
         s = 0 
-        for i in range(len(self.key)):
-            s += ord(self.key[i])
-        s = s % 26  
+        for char in self.key:
+            s += ord(char)
+        s = s % 26
 
-        for i in range(len(self.text)):
-            char = self.text[i] 
+        for char in self.text: 
             if char not in special_char: 
-                if (char.isupper()): 
+                if char.isupper(): 
                     result += chr((ord(char) + s-65) % 26 + 65) 
-                elif(ord(char)==32): 
+                elif ord(char)==32: 
                     result += ' '
-                else: 
+                else:
                     result += chr((ord(char) + s - 97) % 26 + 97)
             else: 
                 result += special_char[(special_char.index(char)+1)%len(special_char)]
-        self.text = result
+        
+        for i, char in enumerate(result):
+            c = chr(ord(char) ^ i ^ ord(self.key[ i % len(self.key)]))
+            self.encrypted += c
+        
 
-    def originalText(self): 
-        special_char = list('[@_!#$%^&*()<>?/\|}{~:+-]') 
+    def decrypt(self):
+        for i, char in enumerate(self.encrypted):
+            c = chr(ord(char) ^ ord(self.key[ i % len(self.key)]) ^ i )
+            self.text += c
+
+        special_char = list('[@_!#$%^&*()<>?/\\|}{~:+-]') 
         result = ""
         s = 0 
-        for i in range(len(self.key)):
-            s += ord(self.key[i])
+        for char in self.key:
+            s += ord(char)
         s = 26 - (s % 26)  
-
-        for i in range(len(self.encrypted)):
-            char = self.encrypted[i]
+        
+        for char in self.text:
             if char not in special_char: 
-                if (char.isupper()): 
+                if char.isupper(): 
                     result += chr((ord(char) + s-65) % 26 + 65) 
-                elif(ord(char)==32): 
+                elif ord(char)==32: 
                     result += ' '
                 else:
                     result += chr((ord(char) + s - 97) % 26 + 97)
@@ -46,33 +52,21 @@ class payload:
                 result += special_char[(special_char.index(char)-1)%len(special_char)]
         self.text = result
 
-    def encrypt(self):
-        self.disp()
-        self.cipherText()
-        self.disp()
-        for i, char in enumerate(self.text):
-            c = chr(ord(char) ^ i ^ ord(self.key[ i % len(self.key)]))
-            self.encrypted += c
-        self.disp()
-
-    def decrypt(self):
-        deciper_str = self.encrypted
-        self.encrypted = ''
-        for i, char in enumerate(deciper_str):
-            c = chr(ord(char) ^ ord(self.key[ i % len(self.key)]) ^ i )
-            self.encrypted += c
-        self.disp()
-        self.originalText()
-        self.disp()
 
     def disp(self):
         print(f"Text = {self.text}")
         print(f"Key = {self.key}")
         print(f"Encrypted = {self.encrypted}")
+        print("#############################\n")
 
 
 if __name__ == "__main__":
-    p = payload(text = "Hello my name is Pulkit+-+-", key="group of keys")
-    p.encrypt()   
-    q = payload(encrypted=p.encrypted, key="group of keys")
+    p = payload(text = "Hello my name is Pulkit\\+-+-", key="Nanny")
+    p.disp()
+    p.encrypt()
+    p.disp()
+
+    q = payload(encrypted=p.encrypted, key="Nanny")
+    q.disp()
     q.decrypt()
+    q.disp()
